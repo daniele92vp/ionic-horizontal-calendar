77bdfef5077cadb8f5603d303362ba50681acf28
Rombolà Gianluca <rag@ti8m.ch>
2019-10-15 09:48:17 +0200

    cleaned code, added minimal documentation

--- src/app/ionic-horizontal-calendar/ionic-horizontal-calendar.component.ts
+++ src/app/ionic-horizontal-calendar/ionic-horizontal-calendar.component.ts
@@ -18,41 +18,70 @@ interface CalendarDay {
 @Component({
   selector: 'app-ionic-horizontal-calendar',
   templateUrl: './ionic-horizontal-calendar.component.html',
-  styleUrls: ['./ionic-horizontal-calendar.component.scss']
-  // changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./ionic-horizontal-calendar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IonicHorizontalCalendarComponent {
-  today = moment();
-  daysOfTheWeek: CalendarDay[] = [];
+  /// INTERNAL VARIABLES ///
+  /** Array of CalendarDays currently displayed. */
+  displayedDays: CalendarDay[] = [];
+
+  /** A string representing the localized month selected, for displaying purposes. */
   monthSelected: string;
-  daySelected: CalendarDay;
+
+  /** The selected CalendarDay, for displaying purposes. */
+  currentSelectedDay: CalendarDay;
+
+  /** First date to render, used to build displayedDays. */
   currentFirstDayRendered: Date;
-  panExludedDelta = 0;
-  @Output() nextDayClicked = new EventEmitter<any>();
-  @Output() prevDayClicked = new EventEmitter<any>();
-  @Output() daySelectedEvent = new EventEmitter<any>();
-  @Input() title: string;
-  @Input() daysToExclude = (day: CalendarDay) => day.date.getDay() === 6 || day.date.getDay() === 0;
-  @Input()
-  set locale(loc: any) {
-    moment.updateLocale(loc, {});
-    this.renderDaysOfWeek(this.today);
-  }
-  get localeData(): moment.Locale {
+
+  /// PRIVATES ///
+  /** Internal variable.  */
+  private panExludedDelta = 0;
+
+  /** Current selected locale for moment. */
+  private get localeData(): moment.Locale {
     return moment.localeData();
   }
 
+  /// OUTPUTS ///
+  /** Emits whenever the next day button is pressed. */
+  @Output() nextDayClicked = new EventEmitter<any>();
+
+  /** Emits whenever the prev day button is pressed. */
+  @Output() prevDayClicked = new EventEmitter<any>();
+
+  /** Emits the new selected day whenever it is changed. */
+  @Output() daySelected = new EventEmitter<any>();
+
+  /// INPUTS ///
+  /** Header to display above the calendar. */
+  @Input() title: string;
+
+  /** Callback to select days that should be excluded from selection. */
+  @Input() daysToExclude = (day: CalendarDay) => day.date.getDay() === 6 || day.date.getDay() === 0;
+
+  /** moment's locale */
+  @Input()
+  set locale(loc: string) {
+    moment.updateLocale(loc, {});
+    this.renderDaysOfWeek(moment());
+  }
+  get locale() {
+    return moment.locale();
+  }
+
   get isFirstDayRenderedToday() {
-    return moment(this.daysOfTheWeek[0].date).isSame(moment(), 'day');
+    return moment(this.displayedDays[0].date).isSame(moment(), 'day');
   }
 
   constructor(private cdRef: ChangeDetectorRef) {
-    this.renderDaysOfWeek(this.today);
+    this.renderDaysOfWeek(this.currentFirstDayRendered);
   }
 
   renderDaysOfWeek(startingDay) {
     this.monthSelected = this.localeData.months(moment(startingDay), 'MMMM');
-    this.daysOfTheWeek = Array(7)
+    this.displayedDays = Array(7)
       .fill(7)
       .map((_, i) => {
         const dayToSave = {
@@ -68,30 +97,35 @@ export class IonicHorizontalCalendarComponent {
       });
   }
 
+  /** Moves the calendar forward by one day. */
   nextDayOfWeek() {
-    const firstDayRendered = moment(this.daysOfTheWeek[0].date).add(1, 'days');
+    const firstDayRendered = moment(this.displayedDays[0].date).add(1, 'days');
     this.renderDaysOfWeek(firstDayRendered);
     this.nextDayClicked.emit();
   }
 
+  /** Moves the calendar backward by one day. */
   prevDayOfWeek() {
-    if (!moment(this.daysOfTheWeek[0].date).isSame(moment(), 'day')) {
-      const firstDayRendered = moment(this.daysOfTheWeek[0].date).subtract(1, 'days');
+    if (!moment(this.displayedDays[0].date).isSame(moment(), 'day')) {
+      const firstDayRendered = moment(this.displayedDays[0].date).subtract(1, 'days');
       this.renderDaysOfWeek(firstDayRendered);
       this.prevDayClicked.emit();
     }
   }
 
-  selectDay(day) {
-    this.daySelected = day;
-    this.daySelectedEvent.emit(day);
+  /** Set selected day */
+  selectDay(day: CalendarDay): void {
+    this.currentSelectedDay = day;
+    this.daySelected.emit(day);
   }
 
-  swipeEnd(event) {
+  /** Hadler for when swiping ends. */
+  swipeEnd(): void {
     this.panExludedDelta = 0;
   }
 
-  swipeMove(event) {
+  /** Handler for when swiping is active and changes offset. */
+  swipeMove(event): void {
     if (event.deltaX - this.panExludedDelta > 40) {
       this.prevDayOfWeek();
       this.panExludedDelta += 40;
