e178057403afd586616bc6c26deb8b04cf397686
Rombolà Gianluca <rag@ti8m.ch>
2019-10-15 10:33:36 +0200

    moved rendering from variable based to dynamic

--- src/app/ionic-horizontal-calendar/ionic-horizontal-calendar.component.ts
+++ src/app/ionic-horizontal-calendar/ionic-horizontal-calendar.component.ts
@@ -13,6 +13,7 @@ interface CalendarDay {
   number: number;
   name: string;
   date: Date;
+  moment: moment.Moment;
 }
 
 @Component({
@@ -27,13 +28,15 @@ export class IonicHorizontalCalendarComponent {
   displayedDays: CalendarDay[] = [];
 
   /** A string representing the localized month selected, for displaying purposes. */
-  monthSelected: string;
+  get monthSelected() {
+    return this.localeData.months(this.currentFirstDayRendered.moment, 'MMMM');
+  }
 
   /** The selected CalendarDay, for displaying purposes. */
   currentSelectedDay: CalendarDay;
 
   /** First date to render, used to build displayedDays. */
-  currentFirstDayRendered: Date;
+  currentFirstDayRendered: CalendarDay;
 
   /// PRIVATES ///
   /** Internal variable.  */
@@ -58,6 +61,9 @@ export class IonicHorizontalCalendarComponent {
   /** Header to display above the calendar. */
   @Input() title: string;
 
+  /** The amount of days to display at once. */
+  @Input() dayCount: number;
+
   /** Callback to select days that should be excluded from selection. */
   @Input() daysToExclude = (day: CalendarDay) => day.date.getDay() === 6 || day.date.getDay() === 0;
 
@@ -65,50 +71,53 @@ export class IonicHorizontalCalendarComponent {
   @Input()
   set locale(loc: string) {
     moment.updateLocale(loc, {});
-    this.renderDaysOfWeek(moment());
+    // this.renderDaysOfWeek(moment());
   }
   get locale() {
     return moment.locale();
   }
 
   get isFirstDayRenderedToday() {
-    return moment(this.displayedDays[0].date).isSame(moment(), 'day');
+    return this.currentFirstDayRendered.moment.isSame(moment(), 'day');
   }
 
   constructor(private cdRef: ChangeDetectorRef) {
-    this.renderDaysOfWeek(this.currentFirstDayRendered);
+    this.currentFirstDayRendered = this.generateCalendarDay(new Date());
   }
 
-  renderDaysOfWeek(startingDay) {
-    this.monthSelected = this.localeData.months(moment(startingDay), 'MMMM');
-    this.displayedDays = Array(7)
+  generateCalendarDay(date: moment.MomentInput | CalendarDay, daysToAdd?: number): CalendarDay {
+    let effectiveDate = this.isCalendarDay(date) ? moment(date.moment) : moment(date);
+    // Apply days delta to date we're outputting
+    if (daysToAdd > 0) {
+      effectiveDate = effectiveDate.add(daysToAdd, 'days');
+    } else if (daysToAdd < 0) {
+      effectiveDate = effectiveDate.subtract(-daysToAdd, 'days');
+    }
+
+    return {
+      number: effectiveDate.date(),
+      name: this.localeData.weekdaysShort(effectiveDate),
+      date: effectiveDate.toDate(),
+      moment: effectiveDate
+    };
+  }
+
+  generateRenderDays() {
+    return Array(7)
       .fill(7)
-      .map((_, i) => {
-        const dayToSave = {
-          number: moment(startingDay)
-            .add(i, 'days')
-            .date(),
-          name: this.localeData.weekdaysShort(moment(startingDay).add(i, 'days')),
-          date: moment(startingDay)
-            .add(i, 'days')
-            .toDate()
-        };
-        return dayToSave;
-      });
+      .map((_, i) => this.generateCalendarDay(this.currentFirstDayRendered, i));
   }
 
   /** Moves the calendar forward by one day. */
   nextDayOfWeek() {
-    const firstDayRendered = moment(this.displayedDays[0].date).add(1, 'days');
-    this.renderDaysOfWeek(firstDayRendered);
+    this.currentFirstDayRendered = this.generateCalendarDay(this.currentFirstDayRendered, 1);
     this.nextDayClicked.emit();
   }
 
   /** Moves the calendar backward by one day. */
   prevDayOfWeek() {
-    if (!moment(this.displayedDays[0].date).isSame(moment(), 'day')) {
-      const firstDayRendered = moment(this.displayedDays[0].date).subtract(1, 'days');
-      this.renderDaysOfWeek(firstDayRendered);
+    if (!this.currentFirstDayRendered.moment.isSame(moment(), 'day')) {
+      this.currentFirstDayRendered = this.generateCalendarDay(this.currentFirstDayRendered, -1);
       this.prevDayClicked.emit();
     }
   }
@@ -133,5 +142,14 @@ export class IonicHorizontalCalendarComponent {
       this.nextDayOfWeek();
       this.panExludedDelta -= 40;
     }
+  }
+
+  /// HELPER METHODS ///
+  compareDays(day1: CalendarDay, day2: CalendarDay) {
+    return day1 && day2 && day1.moment && day2.moment && day1.moment.isSame(day2.moment);
+  }
+
+  private isCalendarDay(object: any): object is CalendarDay {
+    return object && object.date && object.number && object.name && object.moment;
   }
 }
